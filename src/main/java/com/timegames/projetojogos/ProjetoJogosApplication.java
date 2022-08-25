4d609e70fa20f1b7eb033ce9aba892c5ad327739
Eyke Mesquita <eyke1313@gmail.com>
2022-08-24 21:13:45 -0300

    Criando o Projeto Spring

---
+++ src/main/java/com/timegames/projetojogos/ProjetoJogosApplication.java
@@ -0,0 +1,13 @@
+package com.timegames.projetojogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timegames/projetojogos/ProjetoJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timegames.projetojogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
